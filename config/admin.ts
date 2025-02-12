b7d2ebc9015fbacdfaf0255cb7d62ddaa7443280
praiseGit34 <praiseasiimire38@gmail.com>
2025-02-13 02:05:01 +0300

    add tokens

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', '85KMxL0F7Mkxc70n+grA4x1AjrVQy6MgbAi/LEV0e98WiZaPxzv5OPpcGbHMGQuHqY191vNYGRulv+Etr2JJ+A=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'mo1L0Cm7+XMDguXtBI9O8w=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'fCD75FxZpuDFtmeAMpAcvA=='),
     },
   },
   flags: {
