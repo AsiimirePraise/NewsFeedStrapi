dd52b0da3e4229a05c470a73e96ab0cf382b7cc3
praiseGit34 <praiseasiimire38@gmail.com>
2025-02-13 01:45:56 +0300

    add plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
 export default () => ({});
+module.exports = ({ env }) => ({
+    // Other plugin configurations
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'), // Read from environment variable
+      },
+    },
+  });
